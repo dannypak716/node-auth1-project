9f4034e7c083e91d75653035f1deeb46aa452492
Jeong Pak <dannypak716@gmail.com>
2021-12-14 21:30:53 -0500

    when user logs out, cookies and session times out and they no longer have access to database

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -59,7 +59,7 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
   const { password } = req.body
   if (bcrypt.compareSync(password, req.user.password)){
     req.session.user = req.user   // sets cookie on client and server stores a session w/ session id
-    res.json({ message: `Welcome ${req.user.username}` })
+    res.json({ message: `Welcome ${req.user.username}!` })
   } else {
     next({ status: 401, message: 'Invalid credentials' })
   }
@@ -81,7 +81,17 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
   }
  */
 router.get('/logout', (req, res, next) => {
-  res.json('logout')
+  if (req.session.user) {
+    req.session.destroy(err => {
+      if (err) {
+        next(err)
+      } else {
+        res.json({ message: "logged out" })
+      }
+    })
+  } else {
+    res.json({ message: "no session" })
+  }
 })
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
